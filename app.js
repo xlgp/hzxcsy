0215380152c5186d2d3a43b8a1b5332d21f7309a
xlgp <zhang15733@sina.cn>
2017-09-14 17:16:37 +0800

    增加切换环境的配置

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //提供静态文件,(此项目中使用nginx)
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
   secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
@@ -50,10 +50,16 @@ navs:导航菜单
 app.use(function (req, res, next) {
   // res.locals.user = req.session.user;
   res.locals.navs = optionData.navs;
+  
+  if (req.cookies.isload) {
+    res.cookie('isload', req.cookies.isload);
+  }
   next();
 },(req, res, next)=>{ 
-  // console.log(req);
-  logger.info(req.host, req.hostname, req.ip, req.originalUrl);
+  console.log(req.app.get('env'));
+  
+  logger.info(req.hostname, req.headers['x-real-ip'] || req.ip, req.originalUrl);
+  
   next();
 });
 
@@ -76,9 +82,11 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  let status = err.status || 500;
+  logger.error(status, req.originalUrl, err.message);
+  console.log(err);
   // render the error page
-  res.status(err.status || 500);
+  res.status(status);
   res.render('error');
 });
 
